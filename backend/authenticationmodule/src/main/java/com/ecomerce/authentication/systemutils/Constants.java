398ad6fe935f84c8d5fec5448cf604e376d8fa15
Anandhu-Suresh-Vr <anandhusureshvr@gmail.com>
2024-02-13 21:35:34 +0530

    replace backend

--- backend/authenticationmodule/src/main/java/com/ecomerce/authentication/systemutils/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.ecomerce.authentication.systemutils;
-
-public class Constants {
-	public static final int SUCESS = 200;
-	public static final int FAILED = 101;
-	public static final int INVALID_TOKEN = 301;
-	public static final int INVALID_CAPTCHA = 320;
-	public static final int USER_EXIST = 132;
-}

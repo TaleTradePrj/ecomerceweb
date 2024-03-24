0cbfa9315d67f07184568afbcc3c953cd66abe5b
Mahesh Yet Zio <maheshdeluxe53@gmail.com>
2024-03-24 16:43:21 +0530

    extend pdts + singleProduct

--- frontend/src/context/productContext.js
+++ frontend/src/context/productContext.js
@@ -5,6 +5,7 @@ import reducer from "../Reducer/productReducer";
 const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
+const extendedAPI = "http://localhost:5000/api/admin/list-products"
 
 const initialState = {
   isLoading: false,
@@ -23,7 +24,24 @@ const AppProvider = ({ children }) => {
     try {
       const res = await axios.get(url);
       const products = await res.data;
-      dispatch({ type: "SET_API_DATA", payload: products });
+
+      const res2 = await axios.get(extendedAPI)
+      let extendedProducts = await res2.data
+
+      extendedProducts = extendedProducts.map(pdt => {
+        const {_id, ...rest} = pdt
+
+        return {
+          ...rest,
+          id: _id,
+          price: parseInt(pdt.price)
+        }
+      })
+
+      extendedProducts = [...products, ...extendedProducts]
+      console.log("Extended products: ", extendedProducts)
+
+      dispatch({ type: "SET_API_DATA", payload: extendedProducts });
     } catch (error) {
       dispatch({ type: "API_ERROR" });
     }
@@ -34,8 +52,36 @@ const AppProvider = ({ children }) => {
   const getSingleProduct = async (url) => {
     dispatch({ type: "SET_SINGLE_LOADING" });
     try {
-      const res = await axios.get(url);
-      const singleProduct = await res.data;
+      const res2 = await axios.get(extendedAPI)
+      let extendedPdts = await res2.data
+      let singleProduct = {}
+      const res = await axios.get(url).then(
+        success => {
+          singleProduct = success.data
+        }
+      ).catch(err => {
+        console.log("err:", err.request.status)
+        if(err.request.status === 404){
+          const regex = /[?&]id=([^&#]*)/;
+          const match = regex.exec(url);
+          const pdtid = match && match[1];
+  
+          singleProduct = extendedPdts.find(pdt => pdt._id === pdtid)
+  
+          singleProduct.stock = Math.floor(Math.random() * 6) + 5
+          singleProduct.reviews = Math.floor(Math.random() * 91) + 10
+          singleProduct.stars = ((Math.random() * (5 - 1)) + 1).toFixed(1)
+  
+          const {image, _id, price, ...newSingleProduct} = singleProduct
+          newSingleProduct.image = [{"id":"randomid1","width":1080,"height":650,"url": image,"filename":"prod-1.png","size":1080,"type":"image/png"},{"id":"randomid2","width":1080,"height":650,"url": image,"filename":"prod-2.png","size":1080,"type":"image/png"},{"id":"randomid3","width":1080,"height":650,"url": image,"filename":"prod-3.png","size":1080,"type":"image/png"},{"id":"randomid4","width":1080,"height":650,"url": image,"filename":"prod-4.png","size":1080,"type":"image/png"}]
+          newSingleProduct.id = _id
+          newSingleProduct.price = parseInt(price)
+
+  
+          singleProduct = newSingleProduct
+        }
+      });
+      
       dispatch({ type: "SET_SINGLE_PRODUCT", payload: singleProduct });
     } catch (error) {
       dispatch({ type: "SET_SINGLE_ERROR" });
